07d3b68279c341fb80b9aadd7409a9789225cab2
Hugh O' Reilly <hughoreilly32@gmail.com>
2024-11-07 11:55:48 +0000

    Innocent / guilty buttons now working!

--- new-game-project/bag_spawner.gd
+++ new-game-project/bag_spawner.gd
@@ -1,17 +1,31 @@
 extends Node2D
 
 @onready var BagScene = preload("res://bag.tscn")
-var spawned_bags: Array = []
-var current_bag: Node = null  # To keep track of the currently active bag
+var current_bag: Node2D = null
 
+#spawn the first bag on play
+func _ready() -> void:
+	spawn_bag()
+
+#spawn 1 bag every time you press a button
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("next"):
 		spawn_bag()  
 
 func spawn_bag() -> void:
 	var bag = BagScene.instantiate()
-	bag.position = Vector2(100, 100) 
+	bag.position = Vector2(100, 200) 
 	add_child(bag)
-	print("spawned 1 bag")
-	spawned_bags.append(bag)  
-	current_bag = bag  
+	print("spawned 1 bag") 
+	current_bag = bag
+
+
+func _on_button_guilty_pressed() -> void:
+	current_bag.allowed = false
+	current_bag.move()
+	spawn_bag()
+	
+func _on_button_innocent_pressed() -> void:
+	current_bag.allowed = true
+	current_bag.move()
+	spawn_bag()
--- new-game-project/sprite_2d.gd
+++ new-game-project/sprite_2d.gd
@@ -1,30 +1,25 @@
-extends Sprite2D
+extends Node2D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-@export var is_guilty: bool = true 
-
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+@export var allowed: bool = true 
+@export var active_bag: bool = false 
 
 func _ready() -> void:
-	is_guilty = bool(randf() < 0.3)  
-	
-func _process(delta: float) -> void:
-	if is_guilty:  
-		modulate = Color(1, 0, 0)  
-	else:
-		modulate = Color(0, 1, 0)  
+	allowed = bool(randf() < 0.3)  
+	move()
 
+#SET COLOURS based on guilT
+#func _process(delta: float) -> void:
+	#if allowed:  
+		#modulate = Color(1, 0, 0)  
+	#else:
+		#modulate = Color(0, 1, 0)  
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("next"):  
-		if animation_player.current_animation == "idle":
-			if is_guilty:  
-				animation_player.play("denied")
-			else:  
-				animation_player.play("accepted")
-		else:
-			animation_player.play("enter")  # Play 'enter' animation or other logic
+		move()
 
+#despawn bags which are done / offscreen
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "enter":
 		animation_player.play("idle")
@@ -36,6 +31,15 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "accepted":
 		queue_free()
 
-
+#play animations, move the conveyer belt?
 func move() -> void:
-	print("move func received")
+	if animation_player.current_animation == "idle":
+		active_bag == true
+		if allowed:  
+			animation_player.play("accepted")
+			active_bag == false
+		else:  
+			animation_player.play("denied")
+			active_bag == false
+	else:
+		animation_player.play("enter")  # Play 'enter' animation or other logic
